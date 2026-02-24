ad827b234d8c2ecdc7f727a30adc96ca0d8185f8
khushinikam <khushi.nikam@gammaedge.io>
2026-02-24 18:52:07 +0530

    updated changes

--- database.py
+++ database.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-# user = os.getenv("DB_USER")
-# password = os.getenv("DB_PASSWORD")
-# host = os.getenv("DB_HOST")
-# port = os.getenv("DB_PORT")
-# name = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+name = os.getenv("DB_NAME")
 
 
 Database_url = os.getenv("DB_URL")
